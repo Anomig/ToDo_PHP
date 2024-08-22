f60648ae415c7faf13b0ad6e91c00181bc035162
Anomig <goyvaerts.naomi@hotmail.com>
2024-08-22 21:53:09 +0200

    .

--- add_task.php
+++ add_task.php
@@ -12,12 +12,14 @@ $taskController = new TaskController($pdo);
 $error_message = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $list_id = $_POST['list_id'];
+    // Controleer of list_id leeg is en stel het in op NULL indien dit het geval is
+    $list_id = !empty($_POST['list_id']) ? $_POST['list_id'] : NULL;
     $title = $_POST['task_title'];
     $deadline = $_POST['task_deadline'];
     $status = $_POST['task_status'];
     $comment = $_POST['task_comment'];
 
+    // Probeer de taak toe te voegen
     $result = $taskController->create($list_id, $title, $deadline, $status, $comment);
 
     if ($result === true) {
